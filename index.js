d3cf1df1cfac0bfa947d50892c28dd2017fe4618
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-02-28 08:05:26 -0300

    uibuild

--- index.js
+++ index.js
@@ -8,24 +8,31 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
-})
-
 app.get('/api/notes', (req, res) => {
   Note.find({}).then(notes=>{
     res.json(notes)
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -43,6 +50,34 @@ app.post('/api/notes', (req, res) => {
     res.json(savedNote)
   })
 })
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
