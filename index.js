cee1567ae5469bc178b7c95630fde01f7f0daf40
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-02-27 23:10:51 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,79 +1,51 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
+const cors = require('cors')
+const Note = require('./models/note')
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-
-
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes=>{
+    res.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find((note) => note.id === id)
-  if (note) {
+  Note.findById(req.params.id).then(note => {
     res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter((note) => note.id !== id)
-
   response.status(204).end()
 })
 
 app.post('/api/notes', (req, res) => {
-  const note = req.body
-  if (!note || !note.content) {
-    return res.status(400).json({
-      error: 'note.content is missing',
-    })
+  const body = req.body
+  if (body.content === undefined) {
+    return res.status(400).json({ error: 'content missing' })
   }
 
-  const ids = notes.map((note) => note.id)
-  const maxIds = Math.max(...ids)
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-  const newNote = {
-    id: maxIds + 1,
-    content: note.content,
-    important: note.important || false,
-    date: new Date().toISOString(),
-  }
-
-  notes = notes.concat(newNote)
-
-  res.status(201).json(newNote)
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001 // Usando PORT de entorno si está definido
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
